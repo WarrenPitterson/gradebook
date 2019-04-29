5966e13e4663e48e565c633376252f7fc7e0efe8
warren pitterson <warrenpitterson@gmail.com>
2019-04-29 18:03:47 +0100

    write to file, added

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -37,15 +37,20 @@ namespace Gradebook
 
         public override void AddGrade(double grade)
         {
-            var writer = File.AppendText($"{Name}.txt");
-            writer.WriteLine(grade);
-            writer.Dispose();
+            using (var writer = File.AppendText($"{Name}.txt"))
+            {
+                writer.WriteLine(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
             
         }
 
         public override Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+           throw new NotImplementedException();
         }
     }
 
@@ -105,43 +110,18 @@ namespace Gradebook
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
 
-            foreach (var grade in grades)
+            using (var reader = File.OpenText($"{Name}.txt"))
             {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
+                var line = reader.ReadLine();
+                while (line !=null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
             }
-
-            result.Average /= grades.Count;
-
-            switch (result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
-
-            return result;
+                return result;
 
         }
 
